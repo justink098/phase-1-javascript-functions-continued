5c1841c3a8939d36710185db9339ce241bcf7121
Justin <justin.kiarie@student.moringaschool.com>
2022-08-18 15:48:27 +0300

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(string ="roller-skate" ){
+    return `This Saturday, I want to ${string}!`
+}// code your solution here
+function mondayWork (string = 'go to the office'){
+    return `This Monday, I will ${string}.`
+}
+function wrapAdjective(){
+    let result = wrapAdjective('*')
+    let rest = result("a hard worker")
+    rest = function(){
+        return console.log(`You are ${rest}`)
+    }
+}
